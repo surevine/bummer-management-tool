90e33e2c3aadb0227b86ee0fc28d7f5590b2e280
Lloyd Watkin <lloyd.watkin@surevine.com>
2014-09-24 11:56:19 +0100

    Add basic postgresql schema

---
+++ contrib/postgresql/install.sql
@@ -0,0 +1,19 @@
+-- admins table
+
+CREATE TABLE admins ("jid" TEXT NOT NULL PRIMARY KEY);
+
+-- accounts table
+
+CREATE TABLE accounts ("jid" TEXT NOT NULL,
+       	      "password" TEXT NOT NULL,
+              "domain" TEXT NOT NULL PRIMARY KEY,
+			  "updated" TIMESTAMP);
+
+-- schema version table
+
+CREATE TABLE schema_version (version INT NOT NULL PRIMARY KEY,
+                             "when" TIMESTAMP,
+                             description TEXT);
+
+INSERT INTO schema_version (version, "when", description)
+       VALUES (1, 'now', 'Initial database deployment');
