2ab92587aa39c8ae34b14d8b79a34f411ad4cae3
Lloyd Watkin <lloyd.watkin@surevine.com>
2014-10-02 17:12:23 +0100

    Add example config file

---
+++ config.example.js
@@ -0,0 +1,10 @@
+module.exports = {
+    storage: {
+        type: 'postgresql',
+        postgresql: {
+            username: 'managementconsole',
+            password: 'tellnoone',
+            database: 'managementconsole'
+        }
+    }
+}
